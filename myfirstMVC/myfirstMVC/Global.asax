f8398d1c50055996b19836ef02372ee39eb4218f
HoangBui <hoang27119971@gmail.com>
2018-04-02 09:40:10 +0700

    First

---
+++ myfirstMVC/myfirstMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="myfirstMVC.MvcApplication" Language="C#" %>
---
+++ myfirstMVC/myfirstMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myfirstMVC.Startup))]
+namespace myfirstMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
